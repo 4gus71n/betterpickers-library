5d7a4232c8aa02fbeac21dc31e6e4e35db438fb3
Agustin Tomas Larghi <agustin.tomas.larghi@gmail.com>
2017-10-24 12:06:40 -0300

    changes

--- src/main/java/com/codetroopers/betterpickers/OnDialogDismissListener.java
+++
@@ -1,8 +0,0 @@
-package com.codetroopers.betterpickers;
-
-import android.content.DialogInterface;
-
-public interface OnDialogDismissListener {
-
-    void onDialogDismiss(DialogInterface dialoginterface);
-}
